8a53b6dfb86d4253611b84f346d9074d798ae5d5
Adananali344386730 <aa4835912@gmail.com>
2024-05-05 17:47:18 +0530

    firstCommit

---
+++ findDigit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    if(a%2 == 0){
+        printf("Your number is sum");
+    }
+    else{
+        printf("Your number is odd");
+    }
+}
